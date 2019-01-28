621337b24b585782b37c9831882f1b9fe2d141f7
Benjamin Hysell <ben.hysell@gmail.com>
2019-01-28 12:35:55 -0500

    Create dev_Full.ps1

---
+++ dev_Full.ps1
@@ -0,0 +1,69 @@
+# https://github.com/Microsoft/windows-dev-box-setup-scripts
+# Description: Boxstarter Script
+# Author: Microsoft
+# Common dev settings for desktop app development
+
+Disable-UAC
+
+# Get the base URI path from the ScriptToCall value
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
+
+function executeScript {
+    Param ([string]$script)
+    write-host "executing $helperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
+}
+
+#--- Setting up Windows ---
+executeScript "SystemConfiguration.ps1";
+executeScript "FileExplorerSettings.ps1";
+executeScript "RemoveDefaultApps.ps1";
+executeScript "CommonDevTools.ps1";
+executeScript "WSL.ps1";
+executeScript "Browsers.ps1";
+
+executeScript "HyperV.ps1";
+RefreshEnv
+executeScript "WSL.ps1";
+RefreshEnv
+executeScript "Docker.ps1";
+
+#--- Tools ---
+code --install-extension msjsdiag.debugger-for-chrome
+# code --install-extension msjsdiag.debugger-for-edge
+
+#--- Tools ---
+#--- Installing VS and VS Code with Git
+# See this for install args: https://chocolatey.org/packages/VisualStudio2017Community
+# https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community
+# https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio#list-of-workload-ids-and-component-ids
+# visualstudio2017community
+# visualstudio2017professional
+# visualstudio2017enterprise
+
+choco install -y visualstudio2017enterprise --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
+Update-SessionEnvironment #refreshing env due to Git install
+
+#--- UWP Workload and installing Windows Template Studio ---
+coco install -y visualstudio2017-workload-netweb
+coco install -y sql-server-2017
+coco install -y sql-server-management-studio
+
+choco install -y nodejs # Node.js Current, Latest features
+choco install -y visualstudio2017buildtools
+choco install -y visualstudio2017-workload-vctools
+choco install -y python2 # Node.js requires Python 2 to build native modules
+choco install -y yarn
+
+#--- reenabling critial items ---
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
